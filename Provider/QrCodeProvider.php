d36447dfde298b1a12bd56e56cc7849975da37a6
NadirZenith <2cb.md2@gmail.com>
2016-10-14 15:31:08 +0200

    udpate qrcode provider

--- Provider/QrCodeProvider.php
+++ Provider/QrCodeProvider.php
@@ -1,36 +1,74 @@
 <?php
 
-/*
- * This file is part of the Sonata project.
- *
- * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Nz\SonataMediaBundle\Provider;
 
+use Imagine\Image\ImagineInterface;
+use Sonata\CoreBundle\Validator\ErrorElement;
+use Sonata\MediaBundle\Provider\BaseProvider;
+use Sonata\MediaBundle\Provider\ImageProvider;
+use Symfony\Component\Form\FormBuilder;
+use Endroid\QrCode\QrCode;
 use Sonata\CoreBundle\Model\Metadata;
 use Sonata\MediaBundle\Model\MediaInterface;
-use Sonata\MediaBundle\Provider\FileProvider;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sonata\AdminBundle\Form\FormMapper;
+use Gaufrette\Filesystem;
+use Sonata\MediaBundle\CDN\CDNInterface;
+use Sonata\MediaBundle\Generator\GeneratorInterface;
+use Sonata\MediaBundle\Thumbnail\ThumbnailInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\HttpFoundation\File\File;
 
-
-class QrCodeProvider extends FileProvider
+class QrCodeProvider extends ImageProvider
 {
+
+    protected $config = array(
+        'extension'        => QrCode::IMAGE_TYPE_PNG,
+        'size'             => 300,
+        'padding'          => 10,
+        'error_correction' => 'high',
+        'foreground'       => array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0),
+        'background'       => array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0),
+        'label'            => 'Scan the code',
+        'label_size'       => 16,
+        'logo'             => false,
+    );
+
+    /**
+     * @param string $name
+     * @param Filesystem $filesystem
+     * @param CDNInterface $cdn
+     * @param GeneratorInterface $pathGenerator
+     * @param ThumbnailInterface $thumbnail
+     */
+    public function __construct($name, Filesystem $filesystem, CDNInterface $cdn, GeneratorInterface $pathGenerator, ThumbnailInterface $thumbnail, ImagineInterface $adapter)
+    {
+
+        $allowedMimeTypes = array('image/png');
+        $allowedExtensions = array('png');
+        parent::__construct($name, $filesystem, $cdn, $pathGenerator, $thumbnail, $allowedExtensions, $allowedMimeTypes, $adapter);
+
+    }
+
+    public function mergeConfig(array $config = array())
+    {
+        $this->config = array_merge($this->config, $config);
+
+        return $this->config;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function buildEditForm(FormMapper $formMapper)
     {
-        $formMapper->add('name');
+        $formMapper->add('name', null, array('read_only' => true));
         $formMapper->add('enabled', null, array('required' => false));
         $formMapper->add('authorName');
         $formMapper->add('cdnIsFlushable');
         $formMapper->add('description');
         $formMapper->add('copyright');
-        $formMapper->add('binaryContent', 'text', array('required' => false));
+        $formMapper->add('binaryContent', 'text', array('required' => false, 'label' => 'New QrCode text'));
     }
 
     /**
@@ -46,162 +84,49 @@ class QrCodeProvider extends FileProvider
         ));
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getProviderMetadata()
-    {
-        return new Metadata($this->getName(), $this->getName() . '.description', false, 'SonataMediaBundle', array('class' => 'fa fa-qrcode'));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getHelperProperties(MediaInterface $media, $format, $options = array())
-    {
-
-//        $box = $this->getBoxHelperProperties($media, $format, $options);
-//
-//        $player_parameters = array_merge([], isset($options['player_parameters']) ? $options['player_parameters'] : array(), array(
-//            'width' => $box->getWidth(),
-//            'height' => $box->getHeight(),
-//        ));
-//
-//        $params = array(
-//            'player_parameters' => $player_parameters,
-//        );
-//
-//        return $params;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function fixBinaryContent(MediaInterface $media)
-    {
-        if (!$media->getBinaryContent()) {
-            return;
-        }
-
-        if (strlen($media->getBinaryContent()) === 11) {
-            return;
-        }
-
-        if (preg_match("/videos\.sapo\.pt\/([A-Za-z0-9]+)(\/mov\/)?/", $media->getBinaryContent(), $matches)) {
-            $media->setBinaryContent($matches[1]);
-        }
-    }
 
     /**
      * {@inheritdoc}
      */
     protected function doTransform(MediaInterface $media)
     {
-        $this->fixBinaryContent($media);
 
-        if (!$media->getBinaryContent()) {
-            return;
-        }
+        $media->setName($media->getBinaryContent());
 
-        $media->setProviderName($this->name);
-        $media->setProviderStatus(MediaInterface::STATUS_OK);
-        $media->setProviderReference($media->getBinaryContent());
+        $path = $this->createQrCode($media);
+        d($path);
+        $media->setBinaryContent($path);
 
-        $this->updateMetadata($media, true);
+        parent::doTransform($media);
+
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getMetadataThumbnail(MediaInterface $media)
+    protected function createQrCode(MediaInterface $media)
     {
-        $url = sprintf('http://videos.sapo.pt/%s', $media->getProviderReference());
-        try {
-            $html = $this->browser->get($url)->getContent();
 
-            /*
-              $c = curl_init($url);
-              curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-              //curl_setopt(... other options you want...)
+        $path = tempnam(sys_get_temp_dir(), 'sonata_media_qrcode_reference') . '.' . $this->config['extension'];
 
-              $html = curl_exec($c);
-              if (curl_error($c)){
-              die(curl_error($c));
-              }
+        $qrCode = new QrCode();
 
-              // Get the status code
-              $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+        $qrCode
+            ->setText($media->getBinaryContent())
+            ->setSize($this->config['size'])
+            ->setPadding($this->config['padding'])
+            ->setErrorCorrection($this->config['error_correction'])
+            ->setForegroundColor($this->config['foreground'])
+            ->setBackgroundColor($this->config['background'])
+            ->setLabel($this->config['label'])
+            ->setLabelFontSize($this->config['label_size'])
+            ->setImageType($this->config['extension']);
 
-              curl_close($c);
-             */
-        } catch (\RuntimeException $e) {
-            throw new \RuntimeException('Unable to retrieve the thumbnail information for :' . $url, null, $e);
+        if ($this->config['logo'] && is_file($this->config['logo'])) {
+            $qrCode->setLogo($this->config['logo']);
         }
 
-        /* $crawler = new \Symfony\Component\DomCrawler\Crawler($response->getContent()); */
-        $crawler = new \Symfony\Component\DomCrawler\Crawler($html);
-        $metadata = [];
-        $thumbnail_node = $crawler->filter('link[itemprop="thumbnailUrl"]');
+        $qrCode->save($path);
 
-        if ($thumbnail_node->count() === 1) {
-            //http://thumbs.web.sapo.io/?pic=http://cache04.stormap.sapo.pt/vidstore18/thumbnais/54/88/76/11128693_4b5Bb.jpg&crop=center&tv=2&W=1280&H=960&errorpic=http://assets.web.sapo.io/sapovideo/sv/20150903/imgs/playlist_default_thumb_error_pt.gif
+        return $path;
 
-            $thumbnail_url = $thumbnail_node->getNode(0)->getAttribute('href');
-            $parsed_url = parse_url($thumbnail_url);
-
-            $data = [];
-            parse_str($parsed_url['query'], $data);
-
-            if (isset($data['pic'])) {
-                $metadata['thumbnail_url'] = $data['pic'];
-            }
-        }
-
-        if (empty($metadata)) {
-            throw new \RuntimeException('Unable to decode the video information for :' . $url);
-        }
-
-        return $metadata;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function updateMetadata(MediaInterface $media, $force = false)
-    {
-        //http://api.soundcloud.com/resolve?url=http://soundcloud.com/matas/hobnotropic&client_id=656c5a7c166b49062f31fbf24eb13fcd
-        /* $url = sprintf('http://videos.sapo.pt/oembed?url=http://videos.sapo.pt/%s&format=json', $media->getProviderReference()); */
-        /* $url = sprintf('http://api.soundcloud.com/resolve?url=%s', $media->getProviderReference()); */
-        /* $url = sprintf('http://soundcloud.com/oembed?url=%s&format=json&client_id=%s', $media->getProviderReference(), '656c5a7c166b49062f31fbf24eb13fcd'); */
-        $url = sprintf('http://soundcloud.com/oembed?url=%s&format=json', $media->getProviderReference());
-        try {
-            $metadata = $this->getMetadata($media, $url);
-            /* d($metadata); */
-            /* $metadata_thumbnail = $this->getMetadataThumbnail($media); */
-            /* $metadata = array_merge($metadata, $metadata_thumbnail); */
-        } catch (\RuntimeException $e) {
-
-            $media->setEnabled(false);
-            $media->setProviderStatus(MediaInterface::STATUS_ERROR);
-
-            return;
-        }
-        $media->setProviderMetadata($metadata);
-
-        if ($force) {
-            $media->setName($metadata['title']);
-            $media->setAuthorName($metadata['author_name']);
-        }
-
-        $media->setHeight($metadata['height']);
-        $media->setWidth($metadata['width']);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getDownloadResponse(MediaInterface $media, $format, $mode, array $headers = array())
-    {
-        return new RedirectResponse(sprintf('http://videos.sapo.pt/%s', $media->getProviderReference()), 302, $headers);
-    }
 }
